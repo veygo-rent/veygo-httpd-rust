d7e68c7dfb3ac543375c7bf7a436943f68c4c611
Shenghong <me@zhoud2001.io>
2025-02-26 03:57:32 -0500

    Refactor token validation in `get_payment_methods`.
    Streamlined token validation by replacing custom logic with `tokens::verify_user_token`. Reduced redundancy and improved clarity by utilizing helper methods for token operations and database interactions. This refactor enhances maintainability and simplifies error handling.

--- src/api/v1/user/get_payment_methods.rs
+++ src/api/v1/user/get_payment_methods.rs
@@ -1,12 +1,11 @@
-use chrono::{Utc};
-use diesel::{prelude::*, ExpressionMethods, RunQueryDsl};
+use diesel::{prelude::*};
 use serde_derive::{Deserialize, Serialize};
 use warp::Filter;
 use tokio::task::{spawn_blocking};
 use warp::http::StatusCode;
 use crate::db;
+use crate::methods::tokens;
 use crate::model::{AccessToken, PaymentMethod, PublishPaymentMethod};
-use crate::schema;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct GetPaymentMethodsRequestBody {
@@ -21,79 +20,33 @@ pub fn get_payment_methods() -> impl Filter<Extract = (impl warp::Reply,), Error
         .and(warp::header::optional::<String>("x-client-type"))
         .and_then(move |request_body: GetPaymentMethodsRequestBody, client_type: Option<String>| {
             async move {
-                // convert the access token String
-                use schema::access_tokens::dsl::*;
-                let _token_try_decode = hex::decode(request_body.token.clone());
-                let mut _token: Vec<u8>;
-                match _token_try_decode {
+                let if_token_valid_result = tokens::verify_user_token(request_body.user_id.clone(), request_body.token.clone()).await;
+                match if_token_valid_result {
                     Err(_) => {
                         let error_msg = serde_json::json!({"token": &request_body.token, "error": "Token not in hex format"});
-                        return Ok::<_, warp::Rejection>((warp::reply::with_status(warp::reply::json(&error_msg), StatusCode::NOT_ACCEPTABLE),));
-                    }
-                    Ok(token_u8) => {
-                        _token = token_u8;
-                    }
-                }
-
-                // get access_token object
-                let user_id_clone = request_body.user_id.clone();
-                let token_in_db_result = spawn_blocking(move || {
-                    access_tokens.filter(user_id.eq(user_id_clone)).filter(token.eq(_token)).first::<AccessToken>(&mut db::get_connection_pool().get().unwrap())
-                }).await;
-
-                // check access token
-                match token_in_db_result {
-                    Err(_) => {
-                        let error_msg = serde_json::json!({"status": "error", "message": "Internal server error"});
-                        Ok::<_, warp::Rejection>((warp::reply::with_status(warp::reply::json(&error_msg), StatusCode::INTERNAL_SERVER_ERROR),))
-                    }
-                    Ok(Err(_)) => {
-                        let error_msg = serde_json::json!({"token": &request_body.token, "error": "Token invalid"});
                         Ok::<_, warp::Rejection>((warp::reply::with_status(warp::reply::json(&error_msg), StatusCode::NOT_ACCEPTABLE),))
                     }
-                    Ok(Ok(token_in_db)) => {
-                        let token_exp = token_in_db.exp;
-                        if token_exp >= Utc::now() {
-                            // valid token
-                            // generate new token
-                            let new_token = crate::methods::tokens::gen_token_object(user_id_clone, client_type).await;
-                            diesel::delete(access_tokens.filter(id.eq(token_in_db.id))).get_result::<AccessToken>(&mut db::get_connection_pool().get().unwrap()).unwrap();
-                            let new_token_query_result = diesel::insert_into(access_tokens).values(new_token).get_result::<AccessToken>(&mut db::get_connection_pool().get().unwrap());
-                            match new_token_query_result {
-                                Err(_) => {
-                                    let error_msg = serde_json::json!({"status": "error", "message": "Internal server error"});
-                                    Ok::<_, warp::Rejection>((warp::reply::with_status(warp::reply::json(&error_msg), StatusCode::INTERNAL_SERVER_ERROR),))
-                                }
-                                Ok(new_token_in_db) => {
-                                    let publish_token = new_token_in_db.to_publish_access_token();
-                                    let payment_method_query_result_async = spawn_blocking(move || {
-                                        use crate::schema::payment_methods::dsl::*;
-                                        payment_methods.filter(renter_id.eq(user_id_clone)).load::<PaymentMethod>(&mut db::get_connection_pool().get().unwrap())
-                                    }).await;
-                                    match payment_method_query_result_async {
-                                        Err(_) => {
-                                            let error_msg = serde_json::json!({"status": "error", "message": "Internal server error"});
-                                            Ok::<_, warp::Rejection>((warp::reply::with_status(warp::reply::json(&error_msg), StatusCode::INTERNAL_SERVER_ERROR),))
-                                        }
-                                        Ok(Ok(payment_results)) => {
-                                            let payments: Vec<PublishPaymentMethod> = payment_results.iter().map(|x| x.to_public_payment_method().clone()).collect();
-                                            let msg = serde_json::json!({
-                                                "access_token": publish_token,
-                                                "payment_methods": payments,
-                                            });
-                                            Ok::<_, warp::Rejection>((warp::reply::with_status(warp::reply::json(&msg), StatusCode::ACCEPTED),))
-                                        }
-                                        Ok(Err(_)) => {
-                                            let error_msg = serde_json::json!({"status": "error", "message": "Internal server error"});
-                                            Ok::<_, warp::Rejection>((warp::reply::with_status(warp::reply::json(&error_msg), StatusCode::INTERNAL_SERVER_ERROR),))
-                                        }
-                                    }
-                                }
-                            }
-                        } else {
-                            // expired token
-                            let error_msg = serde_json::json!({"token": &request_body.token, "error": "Token invalid"});
+                    Ok(if_token_valid) => {
+                        if !if_token_valid {
+                            let error_msg = serde_json::json!({"token": &request_body.token, "error": "User or token is invalid"});
                             Ok::<_, warp::Rejection>((warp::reply::with_status(warp::reply::json(&error_msg), StatusCode::NOT_ACCEPTABLE),))
+                        } else {
+                            // Token is valid
+                            tokens::rm_token_by_binary(hex::decode(request_body.token).unwrap()).await;
+                            let new_token = tokens::gen_token_object(request_body.user_id.clone(), client_type.clone()).await;
+                            use crate::schema::access_tokens::dsl::*;
+                            let new_token_in_db_publish = diesel::insert_into(access_tokens).values(&new_token).get_result::<AccessToken>(&mut db::get_connection_pool().get().unwrap()).unwrap().to_publish_access_token();
+                            let id_clone = request_body.user_id.clone();
+                            let payment_method_query_result = spawn_blocking(move || {
+                                use crate::schema::payment_methods::dsl::*;
+                                payment_methods.filter(renter_id.eq(id_clone)).load::<PaymentMethod>(&mut db::get_connection_pool().get().unwrap())
+                            }).await.unwrap().unwrap();
+                            let publish_payment_methods: Vec<PublishPaymentMethod> = payment_method_query_result.iter().map(|x| x.to_public_payment_method().clone()).collect();
+                            let msg = serde_json::json!({
+                                                "access_token": new_token_in_db_publish,
+                                                "payment_methods": publish_payment_methods,
+                                            });
+                            Ok::<_, warp::Rejection>((warp::reply::with_status(warp::reply::json(&msg), StatusCode::OK),))
                         }
                     }
                 }
