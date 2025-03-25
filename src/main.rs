cfc3011277f38144d7baf31e5d4c1d4aad53feff
Danny Zhou <35400555+zhoud2001@users.noreply.github.com>
2025-03-25 12:19:31 -0400

    Test updating

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,6 @@ fn get_connection_pool() -> PgPool {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
-    // Refer to the `r2d2` documentation for more methods to use
-    // when building a connection pool
     Pool::builder()
         .build(manager)
         .expect("Could not build connection pool")
