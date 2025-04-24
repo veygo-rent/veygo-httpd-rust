c47444b06896e6d9ef89f3c21ccb3615622ee58b
Shenghong <me@zhoud2001.io>
2025-04-24 03:49:42 -0400

    Revert "Refactor Twilio integration to use `twilio-openapi` crate"
    This reverts commit b9790e894aac2a224090480cd7a9b91f8af887cd.

--- src/integration/twilio_veygo.rs
+++ src/integration/twilio_veygo.rs
@@ -1,47 +1,15 @@
-use dotenv::dotenv;
 use std::env;
-use twilio_openapi::apis::Error;
-use twilio_openapi::apis::configuration::Configuration;
-use twilio_openapi::apis::default_api::{CreateMessageError, create_message};
-use twilio_openapi::models::ApiPeriodV2010PeriodAccountPeriodMessage;
+use dotenv::dotenv;
+use twilio::{Message, TwilioError, Client, OutboundMessage};
 
 pub async fn send_text(
     to: &str,
     msg: &str,
-) -> Result<ApiPeriodV2010PeriodAccountPeriodMessage, Error<CreateMessageError>> {
+) -> Result<Message, TwilioError> {
     dotenv().ok();
     let tw_acc_sid = env::var("TWILIO_SID").expect("TWILIO_SID must be set");
     let tw_token = env::var("TWILIO_AUTH_TOKEN").expect("TWILIO_AUTH_TOKEN must be set");
-    let twilio_config = Configuration {
-        basic_auth: Some((tw_acc_sid.clone(), Some(tw_token))),
-        ..Default::default()
-    };
-    let message = create_message(
-        &twilio_config,
-        &tw_acc_sid,
-        to,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        Option::from("+18334683946"),
-        None,
-        Option::from(msg),
-        None,
-    )
-    .await;
-    message
+    let client = Client::new(&*tw_acc_sid, &*tw_token);
+    let result = client.send_message(OutboundMessage::new("+18334683946", to, msg)).await;
+    result
 }
