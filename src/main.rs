f4f73a27e77a095748f8d75d5c564b0b2cd867aa
zhoud2001 <szhou@veygo.rent>
2025-05-28 21:34:28 -0400

    Revert "Load `BASE_PATH` from environment and update server paths accordingly."
    This reverts commit 784610e3106293851ad45fe0957f7dab4c03ad37.

--- src/main.rs
+++ src/main.rs
@@ -32,11 +32,9 @@ static POOL: Lazy<PgPool> = Lazy::new(|| get_connection_pool());
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
-    let base_path = env::var("BASE_PATH").expect("DATABASE_URL must be set");
     // routing for the server
     let react_app =
-        warp::fs::dir(base_path.clone() + "/www").or(warp::any().and(warp::fs::file(base_path.clone() + "/www/index.html")));
+        warp::fs::dir("/app/www").or(warp::any().and(warp::fs::file("/app/www/index.html")));
     let httpd = api::api().and(warp::path::end()).or(react_app);
     let args: Vec<String> = env::args().collect();
     let port: u16 = args.get(1).and_then(|s| s.parse().ok()).unwrap_or(8080);
@@ -47,8 +45,8 @@ async fn main() {
     // starting the server
     warp::serve(httpd)
         .tls()
-        .cert_path(base_path.clone() + "/cert/httpd/veygo.rent.pem")
-        .key_path(base_path.clone() + "/cert/httpd/veygo.rent.key")
+        .cert_path("/app/cert/httpd/veygo.rent.pem")
+        .key_path("/app/cert/httpd/veygo.rent.key")
         .run((addr, port))
         .await;
 }
