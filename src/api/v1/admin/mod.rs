f27b06681b5f41ae212203e0d5d9b855cbc10f16
Shenghong <me@zhoud2001.io>
2025-06-17 00:40:04 -0400

    Refactor admin `retrieve` endpoint and update `reqwest` dependency
    Streamlined async closure syntax in `retrieve.rs` for improved readability and consistency. Updated `reqwest` to version `0.12.20` for the latest fixes and improvements.

--- src/api/v1/admin/mod.rs
+++ src/api/v1/admin/mod.rs
@@ -6,7 +6,6 @@ use warp::Filter;
 pub fn api_v1_admin() -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone
 {
     warp::path("admin")
-        .and(login::main()
-        .or(retrieve::main()))
+        .and(login::main().or(retrieve::main()))
         .and(warp::path::end())
 }
