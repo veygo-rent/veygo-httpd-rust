f8e80fcba2d76f0b308b94506667645ce5d17f00
Shenghong <me@zhoud2001.io>
2025-11-15 03:51:14 -0500

    ?????!

--- src/api/v1/policy/get.rs
+++ src/api/v1/policy/get.rs
@@ -21,38 +21,37 @@ pub fn main() -> impl Filter<Extract = (impl Reply,), Error = warp::Rejection> +
         .build();
     warp::path("get")
         .and(warp::path::end())
-        .and(warp::post())
-        .and(warp::body::json())
-        .and_then(async move |get_policy_data: GetPolicyData| {
-            let mut pool = POOL.get().unwrap();
-            use schema::policies::dsl as policy_query;
-            let result =
-                if let Some(effective_date) = get_policy_data.effective_date {
-                    policy_query::policies
-                        .filter(policy_query::policy_type.eq(&get_policy_data.policy_type))
-                        .filter(policy_query::policy_effective_date.le(effective_date))
-                        .order(policy_query::policy_effective_date.desc())
-                        .first::<model::Policy>(&mut pool)
-                } else {
-                    policy_query::policies
-                        .filter(policy_query::policy_type.eq(&get_policy_data.policy_type))
-                        .filter(policy_query::policy_effective_date.le(today))
-                        .order(policy_query::policy_effective_date.desc())
-                        .first::<model::Policy>(&mut pool)
-                };
-            match result {
-                Ok(policy) => {
-                    Ok::<_, warp::Rejection>((with_status(warp::reply::json(&policy), StatusCode::OK),))
-                },
-                Err(_) => {
-                    Ok::<_, warp::Rejection>((with_status(warp::reply::json(&"No policy found"), StatusCode::NOT_FOUND),))
-                }
-            }
-        })
-        .with(&cors)
-        .or(warp::path("get")
-            .and(warp::path::end())
-            .and(warp::options())
-            .map(warp::reply).with(&cors)
+        .and(
+            warp::post()
+                .and(warp::body::json())
+                .and_then(async move |get_policy_data: GetPolicyData| {
+                    let mut pool = POOL.get().unwrap();
+                    use schema::policies::dsl as policy_query;
+                    let result =
+                        if let Some(effective_date) = get_policy_data.effective_date {
+                            policy_query::policies
+                                .filter(policy_query::policy_type.eq(&get_policy_data.policy_type))
+                                .filter(policy_query::policy_effective_date.le(effective_date))
+                                .order(policy_query::policy_effective_date.desc())
+                                .first::<model::Policy>(&mut pool)
+                        } else {
+                            policy_query::policies
+                                .filter(policy_query::policy_type.eq(&get_policy_data.policy_type))
+                                .filter(policy_query::policy_effective_date.le(today))
+                                .order(policy_query::policy_effective_date.desc())
+                                .first::<model::Policy>(&mut pool)
+                        };
+                    match result {
+                        Ok(policy) => {
+                            Ok::<_, warp::Rejection>((with_status(warp::reply::json(&policy), StatusCode::OK),))
+                        },
+                        Err(_) => {
+                            Ok::<_, warp::Rejection>((with_status(warp::reply::json(&"No policy found"), StatusCode::NOT_FOUND),))
+                        }
+                    }
+                })
+                .or(warp::options()
+                    .map(warp::reply).with(&cors)
+                )
         )
 }
