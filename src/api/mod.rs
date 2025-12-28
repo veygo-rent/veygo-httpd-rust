98cf4129112bfa16aa01dcff6a5a68eb0d59bf3e
Shenghong <me@zhoud2001.io>
2025-12-28 15:07:42 -0500

    sorry

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -6,11 +6,10 @@ use warp::Filter;
 
 pub fn api() -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
     warp::path("api")
-        .and(v1::api_v1()
-            .or(header_check::main())
+        .and(
+            v1::api_v1()
+                .or(header_check::main())
+                .or(webhook::webhook())
         )
         .and(warp::path::end())
-        .or(
-            webhook::webhook()
-        )
 }
--- src/api/webhook/mod.rs
+++ src/api/webhook/mod.rs
@@ -3,6 +3,5 @@ use warp::Filter;
 pub fn webhook() -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
     warp::path("webhook")
         .and(warp::path::end())
-        .and(warp::post())
         .map(|| warp::reply::json(&"OK"))
 }
