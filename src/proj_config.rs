cfab83668f3655c3c6588202534289766817aa80
Danny Zhou <me@zhoud2001.io>
2026-03-25 21:58:27 -0400

    calculate fuel price

--- src/proj_config.rs
+++ src/proj_config.rs
@@ -1,7 +1,12 @@
+use rust_decimal::Decimal;
+
 #[allow(dead_code)]
 pub static RSVP_BUFFER: i64 = 15;
 #[allow(dead_code)]
 pub static DEPOSIT_AMOUNT: i64 = 200;
+
+#[allow(dead_code)]
+pub const PRICE_PER_CENT_ON_GAS: Decimal = Decimal::from_parts(150, 0, 0, false, 2);
 
 #[allow(dead_code)]
 pub static MIN_IOS_VERSION: &str = "1.0.1";
